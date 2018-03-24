6b4390a702c11e07c1a7548079e9e64353cc98ba
Ian Brennan <ian.brennan@viget.com>
2018-03-24 13:32:57 -0500

    resume

--- app/main.py
+++ app/main.py
@@ -7,6 +7,10 @@ def hello():
     return "Hello World from Flask in a uWSGI Nginx Docker container with \
      Python"
 
+@app.route("/resume")
+def resume():
+    return "Hello this is the resume page"
+
 
 if __name__ == "__main__":
     # Only for debugging while developing
