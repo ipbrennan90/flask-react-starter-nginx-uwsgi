f3def71c4952aeece57b19a41871a5623baddac8
Ian Brennan <ian.brennan@viget.com>
2018-03-25 01:12:22 -0500

    catch all implemented

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ from flask import Flask, render_template
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 
 
-@app.route('/')
-def index():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
     return render_template("index.html")
 
 @app.route("/resume")
