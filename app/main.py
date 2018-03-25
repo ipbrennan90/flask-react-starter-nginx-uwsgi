69ddd299c4bc52c2b85955169da3e691cd36eb66
Ian Brennan <ian.brennan@viget.com>
2018-03-24 22:54:03 -0500

    render_template

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -8,7 +8,7 @@ app = Flask(__name__, static_folder="../static/dist", template_folder="../static
 @app.route('/')
 def index():
     return render_template("index.html")
-    
+
 @app.route("/resume")
 def resume():
     return "Hello this is the resume page yea"
